85a77f76160f5e307e4cfef661a1ea632974a918
Tyler <tyler.starr413@gmail.com>
2022-09-28 09:51:02 -0600

    Refactored spriteloader to not have a spritesheet class, since we only have one spritesheet

--- src/spriteloader.py
+++ src/spriteloader.py
@@ -6,28 +6,27 @@ SPRITE_HEIGHT = 16
 TEXT_WIDTH = SPRITE_WIDTH // 2
 TEXT_HEIGHT = SPRITE_HEIGHT
 
-class Spritesheet:
-    def __init__(self, filename):
-        self.sheet = pygame.image.load(filename).convert_alpha()
+sheet = pygame.image.load('assets/sprites.png').convert_alpha()
     
-    def get_sprite(self, x, y, width, height, color):
-        '''Automatically loads the image at the specified sprite coordinate.
-        For x=0, y=2, will load image at x=0px, y=32px.'''
-        rect = pygame.Rect(x * width, y * height, width, height)
-        surf = pygame.Surface(rect.size).convert()
-        surf.blit(self.sheet, (0, 0), rect)
-        surf = set_color(surf, color)
-        surf.set_colorkey(constants.SPRITE_BG_COLOR, pygame.RLEACCEL)
-        return surf
-
-main_sheet = Spritesheet('assets/sprites.png')
+def get_sprite(x, y, width, height, color):
+    rect = pygame.Rect(x * width, y * height, width, height)
+    surf = pygame.Surface(rect.size).convert()
+    surf.blit(sheet, (0, 0), rect)
+    surf = set_color(surf, color)
+    surf.set_colorkey(constants.SPRITE_BG_COLOR, pygame.RLEACCEL)
+    return surf
 
 def sprite(x, y, color):
-    sprite = main_sheet.get_sprite(x, y, SPRITE_WIDTH, SPRITE_HEIGHT, color)
+    '''Automatically loads the image at the specified sprite coordinate.
+    For x=0, y=2, will load image at x=0px, y=32px.'''
+    sprite = get_sprite(x, y, SPRITE_WIDTH, SPRITE_HEIGHT, color)
     sprite = pygame.transform.scale(sprite, (constants.TILE_SCALE, constants.TILE_SCALE))
     return sprite
 def text(x, y):
-    sprite = main_sheet.get_sprite(x, y, TEXT_WIDTH, TEXT_HEIGHT)
+    '''Automatically loads the image at the specified sprite coordinate.
+    Scales for the half-width of text images.
+    For x=1, y=1, will load image at x=8px, y=16px.'''
+    sprite = get_sprite(x, y, TEXT_WIDTH, TEXT_HEIGHT)
     sprite = pygame.transform.scale(sprite, (constants.TILE_SCALE // 2, constants.TILE_SCALE))
     return sprite
 
